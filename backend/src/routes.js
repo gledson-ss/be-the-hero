d7f9fab6d5b3b5ba1ca6abbdb221116366c29fbd
GledsonS831 <glegogle84@gmail.com>
2020-03-26 15:01:29 -0300

    feat: :pencil: adding last functions in the backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const OngController = require('./controllenrs/OngController');
 const IncidentController = require('./controllenrs/IncidentController');
+const ProfileController = require('./controllenrs/ProfileController');
+const SessionController = require('./controllenrs/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index);
+
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.post('/sessions', SessionController.create);
 module.exports = routes;
